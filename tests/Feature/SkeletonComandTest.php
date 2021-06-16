83d7dc49ce35ba12b74a45abe26e9b7705bfe36f
Vasile Papuc <vasile.papuc@binarcode.com>
2021-06-16 11:56:59 +0300

    test for laravel 7 and 8

---
+++ tests/Feature/SkeletonComandTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace VendorName\Skeleton\Tests\Feature;
+
+
+use VendorName\Skeleton\Tests\TestCase;
+
+class SkeletonComandTest extends TestCase
+{
+    public function test_skeleton_comand_works()
+    {
+        $this->artisan('skeleton')->assertExitCode(0);
+    }
+}
